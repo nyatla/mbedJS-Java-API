5a8576c7b85ab19b926838bca489033dcb545e60
hara41 <hara.shinichi@gmail.com>
2014-08-21 16:56:25 +0900

    5th,executable

--- src.driver/test/hara41/StepperMotor.java
+++ src.driver/test/hara41/StepperMotor.java
@@ -9,75 +9,37 @@ public class StepperMotor extends DriverBaseClass{
 	private Mcu mcu;
 	private DigitalOut CtrlPinA;
 	private DigitalOut CtrlPinB;
-	/*
-	private DigitalOut In1;
-	private DigitalOut In2;
-	private DigitalOut In3;
-	private DigitalOut In4;
-	*/
 	private BusOut bus;
-	public StepperMotor(Mcu i_mcu) throws MbedJsException
+	private int[] mstep = {0xc,0x6,0x3,0x9};
+	private int[] pstep = {0x9,0x3,0x6,0xc};
+	
+	public StepperMotor(Mcu i_mcu,int i_ctrlA,int i_ctrlB ,
+			int i_in1,int i_in2,int i_in3, int i_in4) throws MbedJsException
 	{
 		this.mcu = i_mcu;
-		this.CtrlPinA = new DigitalOut(this.mcu,PinName.p16);
-		this.CtrlPinB = new DigitalOut(this.mcu,PinName.p17);
-		/*
-		this.In1 = new DigitalOut(this.mcu ,PinName.p15 );
-		this.In2 = new DigitalOut(this.mcu ,PinName.p19 );
-		this.In3 = new DigitalOut(this.mcu ,PinName.p18 );
-		this.In4 = new DigitalOut(this.mcu ,PinName.p20 );
-		*/
-		this.bus = new BusOut(this.mcu,PinName.p15,PinName.p19,PinName.p18,PinName.p20);
+		this.CtrlPinA = new DigitalOut(this.mcu,i_ctrlA);
+		this.CtrlPinB = new DigitalOut(this.mcu,i_ctrlB);
+		this.bus = new BusOut(this.mcu,i_in1,i_in2,i_in3,i_in4);
 		
 		this.CtrlPinA.write(1);
 		this.CtrlPinB.write(1);
-		
-		int[] mstep = {0xc,0x6,0x3,0x9};
-		for (int j=0;j<200;j++)
-		{
-			this.bus.write(mstep[j%4]);
-			this.sleep_ms(10);
+	}
+	public void rotate(int i_step , int i_wait_ms) throws MbedJsException
+	{
+		int s = Math.abs(i_step);
+		if(i_step>0){
+			for (int j=0;j<s;j++)
+			{
+				this.bus.write(mstep[j%4]);
+				this.sleep_ms(i_wait_ms);
+			}
+		}else{
+			for (int j=0;j<s;j++)
+			{
+				this.bus.write(pstep[j%4]);
+				this.sleep_ms(i_wait_ms);
+			}
 		}
-		/*
-		for (int i=0;i<8;i++){
-			this.bus.write(0xc);
-			this.sleep_ms(1);
-			this.bus.write(0x6);
-			this.sleep_ms(1);
-			this.bus.write(0x3);
-			this.sleep_ms(1);
-			this.bus.write(0x9);
-			this.sleep_ms(1);
-			
-			//4			
-			this.In1.write(1);
-			this.In2.write(1);
-			this.In3.write(0);
-			this.In4.write(0);		
-			this.sleep_ms(10);
-
-			//3
-			this.In1.write(0);
-			this.In2.write(1);
-			this.In3.write(1);
-			this.In4.write(0);
-			this.sleep_ms(10);
-			//2
-			this.In1.write(0);
-			this.In2.write(0);
-			this.In3.write(1);
-			this.In4.write(1);
-			this.sleep_ms(10);
-			
-			//1
-			this.In1.write(1);
-			this.In2.write(0);
-			this.In3.write(0);
-			this.In4.write(1);
-			this.sleep_ms(10);
-			
-		}
-		*/
 		
 	}
 	public void close() throws MbedJsException
@@ -85,19 +47,16 @@ public class StepperMotor extends DriverBaseClass{
 		this.CtrlPinA.write(0);
 		this.CtrlPinB.write(0);
 		this.bus.write(0);
-	
-	
 	}
 	public void dispose() throws MbedJsException
 	{
-		this.CtrlPinA.write(0);
-		this.CtrlPinB.write(0);
-	
-	
+		this.close();
 	}
 	public static void main(String[] args) throws MbedJsException {
 		Mcu mcu =new Mcu("10.0.0.2");
-		StepperMotor sm = new StepperMotor(mcu);
+		StepperMotor sm = new StepperMotor(mcu,PinName.p16,PinName.p17,
+				PinName.p15,PinName.p19,PinName.p18,PinName.p20);
+		sm.rotate(200, 20);
 		sm.close();
 		mcu.close();
 		System.out.println("done");
