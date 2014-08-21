de8b224eaabc36544011b5ad5e97361d7867f023
hara41 <hara.shinichi@gmail.com>
2014-08-21 12:42:02 +0900

    2nd,testing

--- src.driver/test/hara41/StepperMotor.java
+++ src.driver/test/hara41/StepperMotor.java
@@ -1,10 +1,88 @@
 package test.hara41;
 
-public class StepperMotor {
+import jp.nyatla.mimic.mbedjs.*;
+import jp.nyatla.mimic.mbedjs.javaapi.*;
+import jp.nyatla.mimic.mbedjs.javaapi.driver.utils.DriverBaseClass;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class StepperMotor extends DriverBaseClass{
 
+	private Mcu mcu;
+	private DigitalOut CtrlPinA;
+	private DigitalOut CtrlPinB;
+	private DigitalOut In1;
+	private DigitalOut In2;
+	private DigitalOut In3;
+	private DigitalOut In4;
+	
+	public StepperMotor(Mcu i_mcu) throws MbedJsException
+	{
+		this.mcu = i_mcu;
+		this.CtrlPinA = new DigitalOut(this.mcu,PinName.p22);
+		this.CtrlPinB = new DigitalOut(this.mcu,PinName.p23);
+		this.In1 = new DigitalOut(this.mcu ,PinName.p21 );
+		this.In2 = new DigitalOut(this.mcu ,PinName.p24 );
+		this.In3 = new DigitalOut(this.mcu ,PinName.p25 );
+		this.In4 = new DigitalOut(this.mcu ,PinName.p26 );
+		
+		this.CtrlPinA.write(1);
+		this.CtrlPinB.write(1);
+		
+		this.sleep_ms(1000);
+		for (int i=0;i<1;i++){
+			this.In1.write(1);
+			this.In2.write(0);
+			this.In3.write(0);
+			this.In4.write(1);
+			this.sleep_ms(1000);
+			
+			this.In1.write(0);
+			this.In2.write(0);
+			this.In3.write(1);
+			this.In4.write(1);
+			this.sleep_ms(1000);
+			
+			this.In1.write(0);
+			this.In2.write(1);
+			this.In3.write(1);
+			this.In4.write(0);
+			this.sleep_ms(1000);
+			
+			this.In1.write(1);
+			this.In2.write(1);
+			this.In3.write(0);
+			this.In4.write(0);
+			this.sleep_ms(1000);
+			
+		}
+		
+		
 	}
-
+	public void close() throws MbedJsException
+	{
+		this.CtrlPinA.write(0);
+		this.CtrlPinB.write(0);
+		this.In1.write(0);
+		this.In2.write(0);
+		this.In3.write(0);
+		this.In4.write(0);
+	
+	}
+	public void dispose() throws MbedJsException
+	{
+		this.CtrlPinA.write(0);
+		this.CtrlPinB.write(0);
+		this.In1.write(0);
+		this.In2.write(0);
+		this.In3.write(0);
+		this.In4.write(0);
+	
+	}
+	public static void main(String[] args) throws MbedJsException {
+		Mcu mcu =new Mcu("10.0.0.2");
+		StepperMotor sm = new StepperMotor(mcu);
+		sm.close();
+		mcu.close();
+		System.out.println("done");
+		
+	}
 }
