1822e1b217c668c9fccfd7eca295e7c9bae1a3e2
hara41 <hara.shinichi@gmail.com>
2014-08-30 11:58:25 +0900

    4th,debbugging

--- src.driver/test/hara41/L6470.java
+++ src.driver/test/hara41/L6470.java
@@ -29,7 +29,6 @@ public class L6470 extends DriverBaseClass{
     	this.cs = new DigitalOut(i_mcu , i_cs);
     	this.cs.write(1);
 
-    	//this.resetDevice(); // ok
     	// test code..
         int []ret;	
     	//acc 
@@ -38,22 +37,17 @@ public class L6470 extends DriverBaseClass{
     	//this.view_ret(s0);
     	int [] s1 = {0x03 , 0xe8};
     	this.setParam(L6470.PARAM_ACC, s1, L6470.LENGTH_ACC);
-	
-    	ret = this.getParam(L6470.PARAM_ACC,L6470.LENGTH_ACC);
-    	this.view_ret(ret);
+	    ret = this.getParam(L6470.PARAM_ACC, L6470.LENGTH_ACC);	
     	/*
     	int [] s2 = {0x03 , 0xe8};
-    	this.setParam(L6470.PARAM_DEC, s2, L6470.LENGTH_DEC);
     	this.setParam(L6470.PARAM_DEC, s2, L6470.LENGTH_DEC);
     	
     	int [] s3 = {0x00 , 0x23};
     	this.setParam(L6470.PARAM_MAX_SPEED, s3 , L6470.LENGTH_MAX_SPEED);
-    	this.setParam(L6470.PARAM_MAX_SPEED, s3 , L6470.LENGTH_MAX_SPEED);
     	
     	int [] s4 = {0x00 ,0x00};
     	this.setParam(L6470.PARAM_MIN_SPEED, s4 , L6470.LENGTH_MIN_SPEED);
-    	this.setParam(L6470.PARAM_MIN_SPEED, s4 , L6470.LENGTH_MIN_SPEED);
-    	 */
+    	*/
     	/*
     	this.sendByte(0x69);
     	this.sendByte(0x00);
@@ -61,6 +55,7 @@ public class L6470 extends DriverBaseClass{
     	this.sendByte(0xD0);
     	*/
     	//this.run(1, 100); // ok
+    	this.resetDevice(); // ok
     	//this.move(1, 400); 
     	//this.goTo(100); // ok
     }
@@ -98,17 +93,19 @@ public class L6470 extends DriverBaseClass{
     	if(i_len%8!=0){
     		length ++;
     	}
-    	int[] retval = new int[length];
-    	int[] value = new int[length];
+    	int[] retval = new int[length+1];
+    	int[] value = new int[length+1];
     	
     	value[0] = 0x20 | (i_param & 0x1f);
 
-    	for (int i=1;i<length ; i++){
+    	for (int i=1;i<=length ; i++){
     		value[i]= 0x00;
     	}
     	
     	this.view_ret(value);
     	retval = this.sendRecive(value, length);
+    	this.view_ret(retval);
+    	
     	return retval;
     }
 
@@ -119,11 +116,7 @@ public class L6470 extends DriverBaseClass{
     	for(int i=0;i<i_length;i++){
     		retval[i] = this.sendByte(i_value[i]);    		
     	}
-    	// 2回目　2回書き込まないと値が正常に書き換わらない対策
-    	for(int i=0;i<i_length;i++){
-    		retval[i] = this.sendByte(i_value[i]);    		
-    	}
-
+    	
     	return retval;
     }
     
